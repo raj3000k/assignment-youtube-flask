0ef70e741d69da3035e046b06c43d61063480859
raj3000k <rajmotwani38@gmail.com>
2024-03-29 00:54:39 +0530

    Added youtube api and search function

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,2 @@
 FLASK_ENV=development
-FLASK_APP=flask_youtube
+FLASK_APP=assignment-youtube-flask
--- flask_youtube/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-def crate_app(config_file = 'settings.py'):
-    app = Flask(__name__)
-
-    app.config.from_pyfile(config_file)
-
-    return app
